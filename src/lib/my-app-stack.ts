b30acc709a26bc3a79c60570e4d42455d9da0b53
Jani Haakana <jani.k.haakana@solita.fi>
2022-05-31 22:03:29 +0300

    Use aws-lambda instead of aws-lambda-nodejs

--- src/lib/my-app-stack.ts
+++ src/lib/my-app-stack.ts
@@ -1,4 +1,4 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs'
+import * as lambda from 'aws-cdk-lib/aws-lambda'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
 import { Stack, StackProps } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
@@ -8,13 +8,12 @@ export class MyAppStack extends Stack {
   constructor (scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    const hello = new lambda.NodejsFunction(this, 'Hello', {
-      entry: path.join(__dirname, '../lambda/index.ts'),
-      handler: 'handler',
-      bundling: {
-        environment: {
-          FUNCTION_NAME: 'monitoring-example-MetricsLambda-123456789'
-        }
+    const hello = new lambda.Function(this, 'Hello', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../dist/')),
+      handler: 'index.handler',
+      environment: {
+        FUNCTION_NAME: 'monitoring-example-MetricsLambda-123456789'
       }
     })
 
